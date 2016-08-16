b913ca4b60e44132b0f04ea759dd697d378182c2
Sourabh Shirhatti <shirhatti@outlook.com>
2016-08-16 12:34:19 -0700

    Initial commit

---
+++ src/connectionclose/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace connectionclose.Controllers
+{
+    public class A
+    {
+        public B b { get; set; }
+    }
+    public class B
+    {
+        public A a { get; set; }
+    }
+
+    public class HomeController
+    {
+        [Route("/")]
+        public A Index()
+        {
+            var a = new A
+            {
+                b = null
+            };
+
+            var b = new B
+            {
+                a = a
+            };
+            a.b = b;
+
+            return a;
+        }
+    }
+}
---
+++ src/connectionclose/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace connectionclose
+{
+    public class Program
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Program>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
